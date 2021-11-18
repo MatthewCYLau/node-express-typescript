e351613520731a1896f2e0d5e03cfc24b0cfec99
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-18 12:40:58 +0800

    feat: added auth middleware for todos router

--- src/todo/todo.router.ts
+++ src/todo/todo.router.ts
@@ -1,19 +1,31 @@
 import express, { Request, Response } from "express";
 import { Todo } from "../todo/todo.interface";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/todos", async (req: Request, res: Response) => {
-  const todo = await Todo.find({});
-  return res.status(200).send(todo);
-});
+router.get(
+  "/todos",
+  currentUser,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const todo = await Todo.find({});
+    return res.status(200).send(todo);
+  }
+);
 
-router.post("/todos", async (req: Request, res: Response) => {
-  const { title, description } = req.body;
+router.post(
+  "/todos",
+  currentUser,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { title, description } = req.body;
 
-  const todo = Todo.build({ title, description });
-  await todo.save();
-  return res.status(201).send(todo);
-});
+    const todo = Todo.build({ title, description });
+    await todo.save();
+    return res.status(201).send(todo);
+  }
+);
 
 export { router as todoRouter };
