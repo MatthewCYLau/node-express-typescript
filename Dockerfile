d24f5186d3928cd463e0ca5acc7ef7791d23ae4d
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-18 12:11:52 +0800

    fix: updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
-RUN npm build
+RUN npm run build
 EXPOSE 8080
 CMD ["npm","start"]
--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import express from "express";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
 
